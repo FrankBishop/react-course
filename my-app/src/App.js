70937a30f76dc79f3884a973aebc80e11e3830cd
FrankBishop <ffbishop3@gmail.com>
2021-05-20 19:20:37 -0700

    hooks-practice-2 in progress

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,35 @@
 import './App.css';
 import React, {useState} from "react";
 import {useForm} from "./useForm";
+// import { useEffect } from 'react';
+import {Hello} from "./hello";
 
 function App() {
   const [number, setNumber] = useState(50);
   const [number2, setNumber2] = useState(70);
   const [values, handleChange] = useForm({email: '', password: ''});
 
+  const [showHello, setShowHello] =  useState(true);
+
+  // useEffect( () => {
+  //   console.log('render')
+  // },[values.password]);
+
+  //console.logs once when it mounts
+
+  // useEffect(() => {
+  //   console.log('render')
+  //   return  () => {
+  //     console.log('unmount')
+  //   };
+  // }, []);
+
   return (
     <div>
+      <button onClick={() => setShowHello(!showHello)}>
+        Toggle
+      </button>
+      {showHello && <Hello />}
       <button onClick = {() => {
         setNumber(n => n+1)
         setNumber2(n => n+2)
